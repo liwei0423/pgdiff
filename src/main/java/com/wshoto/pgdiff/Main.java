b7c18f30d5541c82aacb1906004d6b066967322d
fahai <786541343@qq.com>
2022-10-11 15:32:57 +0800

    调整打包版本

--- src/main/java/com/wshoto/pgdiff/Main.java
+++ src/main/java/com/wshoto/pgdiff/Main.java
@@ -1,5 +1,7 @@
 package com.wshoto.pgdiff;
 
+import cn.hutool.log.Log;
+import cn.hutool.log.LogFactory;
 import com.wshoto.pgdiff.util.FileUtils;
 
 import java.io.IOException;
@@ -10,18 +12,26 @@ import java.io.IOException;
  * @date: 2022/10/10
  */
 public class Main {
+    private static final Log log = LogFactory.get();
 
     public static void main(String[] args) throws IOException {
         if (args == null || args.length == 0) {
             System.out.println("please add param: 448、456、output fileDir");
             return;
         } else {
+            log.info("running...");
+            long start = System.currentTimeMillis();
             String srcDir = args[0];
             String destDir = args[1];
             String outDir = args[2];
+            log.info("copy no exist file...");
             FileUtils.copyNoExistFile(srcDir, destDir, outDir);
+            log.info("apg diff...");
             FileUtils.apgdiff(srcDir, destDir, outDir);
+            log.info("remove line...");
             FileUtils.removeLine(outDir);
+            long end = System.currentTimeMillis();
+            log.info("success, use time {}s",(end-start)/1000);
         }
     }
 }
